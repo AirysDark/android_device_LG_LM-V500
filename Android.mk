8805f7ac27345e66bb2344460acfc632df726ae6
AirysDark <47623717+AirysDark@users.noreply.github.com>
2023-06-24 22:15:22 +1000

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE), REL)
+ifeq ($(TARGET_DEVICE), flashlmdd)
 LOCAL_PATH := device/lge/flashlmdd
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
