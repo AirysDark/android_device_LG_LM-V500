86b63e95d07f123d23c2e657d2b7d7e304c1fbc8
AirysDark <47623717+AirysDark@users.noreply.github.com>
2023-06-24 10:05:39 +1000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,11 @@
 DEVICE_PATH := device/LGE/REL
-# For building with TWRP minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
-# Architecture (arm64)
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := Qualcomm
+TARGET_2ND_ARCH := arm
 # Assert
 TARGET_OTA_ASSERT_DEVICE := REL
 # File systems
@@ -24,13 +23,29 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+TARGET_IS_64_BIT := true                      # true/false: Determine if the device is 64-bit or not
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/zImage
+TARGET_PREBUILT_RECOVERY_KERNEL := $(LOCAL_PATH)/prebuilt/zImage
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 # TWRP Configuration
-TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true #Only if you need it
+TW_EXCLUDE_SUPERSU := true                    # true/false: Add SuperSU or not
+TW_INCLUDE_CRYPTO := true                     # true/false: Add Data Encryption Support or not
+TW_INPUT_BLACKLIST := "hbtp_vm"               # Optional: Disables virtual mouse
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_THEME := portrait_hdpi                     # Set the exact theme you wanna use. If resulation doesn't match, define the height/width
+DEVICE_RESOLUTION := 720x1280                 # The Resolution of your Device
+TARGET_SCREEN_HEIGHT := 1280                    # The height
+TARGET_SCREEN_WIDTH := 720                      # The width
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_INCLUDE_NTFS_3G := true                    # Include NTFS Filesystem Support
+TW_INCLUDE_FUSE_EXFAT := true                 # Include Fuse-ExFAT Filesystem Support
+TWRP_INCLUDE_LOGCAT := true                   # Include LogCat Binary
+TW_INCLUDE_FB2PNG := true                     # Include Screenshot Support
+TW_DEFAULT_LANGUAGE := en                     # Set Default Language 
+TW_EXTRA_LANGUAGES := false
